4b736f399829765db2f9a0bfd744a86401c55668
Leandro Dolensi <87672980+LeandroDolensi@users.noreply.github.com>
2023-04-24 16:52:38 -0300

    Dev (#16)
    * finishing SQL connection (#15)

--- src/main/java/br/com/erudio/exceptions/ResourceNotFoundException.java
+++ src/main/java/br/com/erudio/exceptions/ResourceNotFoundException.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.Serial;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsupportedMathOperationException extends RuntimeException {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
 
-    public UnsupportedMathOperationException(String ex){
-        super(ex);
-    }
     @Serial
     private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String ex){
+        super(ex);
+    }
+
 }
 
